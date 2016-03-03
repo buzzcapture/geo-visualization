f73ec181ce91bc231139f3133e110358b99299ad
Stijn Veken <stijnveken@gmail.com>
2016-03-03 11:31:47 +0100

    Placed map on the webpage, made it a nice and boring grey.

--- src/js/components/page.jsx
+++ src/js/components/page.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React from 'react';
+import { render } from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 export default React.createClass({
 
   displayName: "Page",
 
+
   render: function () {
     return (
-      <div>
-        <h1>React-es6-starter-kit</h1>
-        <p>A simple starter kit for your React project.</p>
-      </div>
+        <Map center={[51.505, -0.09]} zoom={13}>
+          <TileLayer
+              url='https://{s}.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3ZpZG8iLCJhIjoiZGI1NmFhNmViNzZlZGNkMjQ3ZjdlMjVkZTMwNmFkNmEifQ.wnMrAkxOObDfpS1-KvPkqw'
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </Marker>
+        </Map>
     );
   }
 
 });
+
