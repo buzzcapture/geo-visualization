d73070263797b4515f6ea9cd0c307cb1b817a8a4
stijn veken <stijnveken@gmail.com>
2016-03-24 12:32:23 +0100

    adding dist

---
+++ dist/leafletmap.js
@@ -0,0 +1,170 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _lodash = require("lodash");
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _color = require("color");
+
+var _color2 = _interopRequireDefault(_color);
+
+var _leaflet = require("leaflet");
+
+var _leaflet2 = _interopRequireDefault(_leaflet);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var TILE_LAYER_URL = 'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3ZpZG8iLCJhIjoiZGI1NmFhNmViNzZlZGNkMjQ3ZjdlMjVkZTMwNmFkNmEifQ.wnMrAkxOObDfpS1-KvPkqw';
+
+var Utils = {
+    getMean: function getMean(values) {
+        return values.reduce(function (a, b) {
+            return a + b;
+        }) / values.length;
+    },
+
+    getStandardDeviation: function getStandardDeviation(values, mean) {
+        var variance = 0;
+
+        values.forEach(function (amount) {
+            variance += Math.pow(mean - amount, 2);
+        });
+
+        return Math.sqrt(variance /= values.length);
+    }
+};
+
+var LeafletMap = function LeafletMap() {
+    this.init.apply(this, arguments);
+};
+
+LeafletMap.prototype = {
+    init: function init(id) {
+        var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+        this.options = _lodash2.default.assign({
+            zoom: 7,
+            center: [52.374030, 4.8896900],
+            baseColor: "#A32020"
+        }, options);
+
+        this.map = _leaflet2.default.map(id).setView(this.options.center, this.options.zoom);
+        _leaflet2.default.tileLayer(TILE_LAYER_URL).addTo(this.map);
+    },
+
+    getStyle: function getStyle(bounds, feature) {
+        return {
+            fillColor: this.getColor(this.options.baseColor, feature.properties.amount, bounds),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    },
+
+    update: function update(data) {
+        this.postingBounds = this.getBounds(data.features);
+        // If the geojson layer already exists remove it first before adding the new one.
+        if (this.map.hasLayer(this.geojson)) {
+            this.map.removeLayer(this.geojson);
+        }
+
+        this.geojson = _leaflet2.default.geoJson(data, {
+            pointToLayer: this.pointToLayer.bind(this)
+        }).addTo(this.map);
+    },
+
+    getBounds: function getBounds(items) {
+        var mean, lowestAmount, // the lowest amount of postings
+        highestAmount, // the highest amount of postings
+        standardDeviation, // the standard deviation
+        values, // all of the values from the response, used for working with the values easier;
+        trimmedValues; // the values after being trimmed
+
+        values = [];
+        items.forEach(function (item) {
+            return values.push(item.properties.amount);
+        });
+
+        mean = Utils.getMean(values);
+        standardDeviation = Utils.getStandardDeviation(values, mean);
+
+        // get a box to use for number trimming
+        lowestAmount = mean - standardDeviation;
+        highestAmount = mean + standardDeviation;
+
+        console.log("High:", highestAmount, "Low:", lowestAmount);
+
+        trimmedValues = _lodash2.default.filter(values, function (value) {
+            return lowestAmount < value && highestAmount > value;
+        });
+
+        //get the new lowest and highest posting amounts
+        lowestAmount = Math.min.apply(Math, _toConsumableArray(trimmedValues));
+        highestAmount = Math.max.apply(Math, _toConsumableArray(trimmedValues));
+
+        return {
+            lowest: lowestAmount,
+            highest: highestAmount
+        };
+    },
+
+    getColor: function getColor(baseColor, amount, bounds) {
+        var total, part, color, offset, percentage, slices;
+        // Make the color with the baseColor
+        color = (0, _color2.default)(baseColor);
+
+        // variables for the percentage calculation
+        total = bounds.highest - bounds.lowest;
+        part = amount - bounds.lowest;
+        slices = 5; // Temporary, will be provided by the user.
+        offset = 100 / slices;
+
+        // Calculate the percentage
+        percentage = Math.floor(part / total * 100);
+        // get the percentage to be between 0-100
+        percentage = Math.min(Math.max(percentage, 0), 100);
+        // Get the the color brackets the percentage is in
+        percentage = Math.floor(percentage / offset);
+        // 1 - (100 / slices * percentage / 100)
+        // 1 to invert the result
+        // 100 / slices to get the percentage per slice
+        // * percent The total percentage
+        // / 100 to get it between 0-1
+        return color.lighten(1 - 100 / slices * percentage / 100).hexString();
+    },
+
+    onFeature: function onFeature(feature, layer) {
+        layer.bindPopup("<b>" + feature.properties.amount + "</b>", { closeButton: false, minWidth: 0 });
+    },
+
+    pointToLayer: function pointToLayer(feature, latlng) {
+        var divText, iconWidth, iconHeight, icon, color, style;
+        color = this.getColor(this.options.baseColor, feature.properties.amount, this.postingBounds);
+        divText = feature.properties.amount.toString();
+        iconWidth = 5 * divText.length + 10;
+        iconHeight = 16;
+        style = 'style="text-align: center; background-color:' + color + '"';
+        icon = _leaflet2.default.divIcon({ iconSize: [iconWidth, iconWidth],
+            iconAnchor: [iconWidth / 2, iconWidth / 2],
+            className: "custom-leaflet-marker",
+            html: "<div " + style + "><b>" + feature.properties.amount + "</b></div>" });
+
+        return _leaflet2.default.marker(latlng, { icon: icon });
+    },
+
+    destroy: function destroy() {}
+};
+
+LeafletMap.create = function () {
+    return new (Function.prototype.bind.apply(LeafletMap, [null].concat(Array.prototype.slice.call(arguments))))();
+};
+
+exports.default = LeafletMap;
---
+++ dist/map.js
@@ -0,0 +1,85 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _leafletmap = require("./leafletmap");
+
+var _leafletmap2 = _interopRequireDefault(_leafletmap);
+
+var _lodash = require("lodash");
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _react2.default.createClass({
+    displayName: "ReactMapComponent",
+
+    propTypes: {
+        data: _react2.default.PropTypes.object,
+        id: _react2.default.PropTypes.string,
+        onZoom: _react2.default.PropTypes.func
+    },
+
+    getInitialState: function getInitialState() {
+        return {
+            accuracy: 6 //
+        };
+    },
+
+    shouldComponentUpdate: function shouldComponentUpdate() {
+        return false;
+    },
+
+    componentDidMount: function componentDidMount() {
+        this.map = _leafletmap2.default.create(this.props.id);
+        this.map.update(this.props.data);
+        this.map.map.on("moveend", _lodash2.default.debounce(this.onUpdate, 1000));
+        this.setAccuracy(this.map.map.getZoom());
+    },
+
+    componentWillUnmount: function componentWillUnmount() {
+        this.map.destroy();
+    },
+
+    componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+        this.map.update(nextProps.data);
+    },
+
+    setAccuracy: function setAccuracy(zoom) {
+        var accuracy;
+        if (zoom <= 7) {
+            accuracy = 3;
+        } else if (zoom <= 9) {
+            accuracy = 4;
+        } else if (zoom <= 12) {
+            accuracy = 5;
+        } else if (zoom <= 13) {
+            accuracy = 6;
+        } else {
+            accuracy = 6;
+        }
+
+        this.setState({
+            accuracy: accuracy
+        });
+    },
+
+    onUpdate: function onUpdate(event) {
+        // You will call the function from this.props.{function} here.
+        // Calculate the accuracy based on the zoom level and pass it to the api.
+        // The response data should come in the form of new props. See componentWillReceiveProps
+        this.setAccuracy(this.map.map.getZoom());
+        console.log(this.state.accuracy);
+    },
+
+    render: function render() {
+        return _react2.default.createElement("div", { ref: "container", id: this.props.id });
+    }
+});
